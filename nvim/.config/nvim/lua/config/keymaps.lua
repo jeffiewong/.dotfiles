4f9811a0fa762ead20cd3d77a7f1ffdfecd0b32a
Jeffie Wong <jeffie512@gmail.com>
2025-10-13 23:28:24 -0400

    Add tmux and nvim configs

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up half page" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down half page" })
+vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent" })
+vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Un-indent" })
+
+vim.keymap.set("i", "kj", "<Esc>", { desc = "Esc" })
+vim.keymap.set("i", "<Tab>", ">>", { desc = "Indent" })
+vim.keymap.set("i", "<S-Tab>", "<<", { desc = "Un-indent" })
+
+vim.keymap.set("v", "<Tab>", ">>", { desc = "Indent" })
+vim.keymap.set("v", "<S-Tab>", "<<", { desc = "Un-indent" })
