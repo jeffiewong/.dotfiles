e0f05edc600ec516939b50e284f857c86a64da60
Jeffie Wong <jeffie512@gmail.com>
2025-10-15 20:20:10 -0400

    Update nvim tab keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,7 @@ vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent" })
 vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Un-indent" })
 
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Esc" })
-vim.keymap.set("i", "<Tab>", ">>", { desc = "Indent" })
-vim.keymap.set("i", "<S-Tab>", "<<", { desc = "Un-indent" })
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { desc = "Un-indent" })
 
-vim.keymap.set("v", "<Tab>", ">>", { desc = "Indent" })
-vim.keymap.set("v", "<S-Tab>", "<<", { desc = "Un-indent" })
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Un-indent" })
