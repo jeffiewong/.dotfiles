0b6a538f6f59eff7e2cb31195e7b0801e9989df8
Jeffie Wong <jeffie512@gmail.com>
2026-03-04 22:32:58 -0500

    nvim update catppuccin config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,20 +3,22 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        auto_integrations = true,
-        integrations = {
-          lualine = true
+    opts = {
+      transparent_background = true,
+      integrations = {
+        snacks = {
+          enabled = true,
+          -- indent_scope_color = "mauve",
+          indent_scope_color = "lavender",
         }
-      })
-
-      local sign = vim.fn.sign_define
-
-      sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-      sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-      sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
-      sign("DapStopped", { text = "➜", texthl = "DapStopped", linehl = "", numhl = ""})
-    end,
+      },
+      highlight_overrides = {
+        all = function(colors)
+          return {
+            DapStoppedLine = { bg = colors.surface0 },
+          }
+        end,
+      }
+    }
   }
 }
