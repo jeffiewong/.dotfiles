6f792251986f199f5051e98bad60e432a2d10290
Jeffie Wong <jeffie512@gmail.com>
2026-03-05 23:37:31 -0500

    nvim update dap breakpoint signs

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -9,6 +9,19 @@ return {
       { "<F12>", function () require("dap").step_out() end, desc = "DAP Step Out" },
       { "<S-F5>", function () require("dap").terminate() end, desc = "DAP Terminate" },
       { "<C-F5>", function () require("dap").restart() end, desc = "DAP restart" },
-    }
+    },
+    opts = function()
+
+      local colors = require("catppuccin.palettes").get_palette()
+      vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = colors.red })
+      vim.api.nvim_set_hl(0, "DapStopped", { fg = colors.yellow })
+
+      vim.schedule(function ()
+        vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
+        vim.fn.sign_define("DapStopped", { text = "➜", texthl = "DapStopped", linehl = "", numhl = ""})
+        -- vim.fn.sign_define("DapBreakpointCondition", { text = "●", linehl = "", numhl = ""})
+        -- vim.fn.sign_define("DapLogPoint", { text = "◆", linehl = "", numhl = ""})
+      end)
+    end,
   }
 }
